63fe7ee58c50a1fee7ad3fef580b7b2ab79cdfe9
Tevis Gehr <tevis.gehr@gmail.com>
2017-05-31 15:07:48 -0600

    Naked Twins working.

--- solution.py
+++ solution.py
@@ -37,15 +37,11 @@ class Board(object):
                 if counts[item] == 2:
                     #eliminate those two numbers from all boxes in the unit
                     twins = list(item)
-                    for peer in self.peers[unit[0]]:
+                    for peer in unit:
                         if values[peer] != item:
                             for digit in twins:
                                 values[peer] = values[peer].replace(digit,'')
         return values
-
-
-
-
 
     def grid_values(self, grid):
         """
@@ -123,6 +119,7 @@ class Board(object):
         for value in values[s]:
             new_sudoku = values.copy()
             new_sudoku[s] = value
+            attempt = self.naked_twins(new_sudoku)
             attempt = self.search(new_sudoku)
             if attempt:
                 return attempt
@@ -154,9 +151,12 @@ class Board(object):
 
 
 if __name__ == '__main__':
-    diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    #diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
     #diag_sudoku_grid = '2..............2....1....7...6..8...3.......7...6......4....8.....2.............3'
     #diag_sudoku_grid = '.........................7......8...3.......7...6................................'
+
+    #'Hard' example found online:
+    diag_sudoku_grid = '....1.8..8..6...5.45.9.3.7....3...9.9.7...4.3.3...1....1.8.4.65.4...6..1..6.7....'
     board = Board(diag_sudoku_grid)
     solution = board.solve()
     board.display(solution)
@@ -169,11 +169,13 @@ if __name__ == '__main__':
     except:
         print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
 
-
+'''
 def cross(A, B):
     "Cross product of elements in A and elements in B."
     return [s+t for s in A for t in B]
 def naked_twins(values):
+    print("Original Board:")
+    Board(values).display(values)
     rows = 'ABCDEFGHI'
     cols = '123456789'
     boxes = cross(rows, cols)
@@ -196,13 +198,50 @@ def naked_twins(values):
     #both of those values can be eliminated from all other boxes in the unit.
     for unit in unitlist:
         two_values = [values[box] for box in unit if len(values[box]) == 2]
+        print(two_values)
         counts = Counter(two_values)
+        #print(counts)
         for item in counts:
             if counts[item] == 2:
+                print(item)
                 #eliminate those two numbers from all boxes in the unit
                 twins = list(item)
-                for peer in peers[unit[0]]:
+                #print(twins)
+                for peer in unit:
+                    #print(peer)
                     if values[peer] != item:
+                        #print(values[peer])
                         for digit in twins:
                             values[peer] = values[peer].replace(digit,'')
+                        print("reduction made")
+                        Board(values).display(values)
+    print("Final Board:")
+    Board(values).display(values)
+
+
+    possible_solutions_1 = [
+        {'G7': '6', 'G6': '3', 'G5': '2', 'G4': '9', 'G3': '1', 'G2': '8', 'G1': '7', 'G9': '5', 'G8': '4', 'C9': '1',
+         'C8': '5', 'C3': '8', 'C2': '237', 'C1': '23', 'C7': '9', 'C6': '6', 'C5': '37', 'A4': '2357', 'A9': '8',
+         'A8': '6', 'F1': '6', 'F2': '4', 'F3': '23', 'F4': '1235', 'F5': '8', 'F6': '125', 'F7': '35', 'F8': '9',
+         'F9': '7', 'B4': '27', 'B5': '1', 'B6': '8', 'B7': '27', 'E9': '2', 'B1': '9', 'B2': '5', 'B3': '6', 'C4': '4',
+         'B8': '3', 'B9': '4', 'I9': '9', 'I8': '7', 'I1': '23', 'I3': '23', 'I2': '6', 'I5': '5', 'I4': '8', 'I7': '1',
+         'I6': '4', 'A1': '1', 'A3': '4', 'A2': '237', 'A5': '9', 'E8': '1', 'A7': '27', 'A6': '257', 'E5': '347',
+         'E4': '6', 'E7': '345', 'E6': '579', 'E1': '8', 'E3': '79', 'E2': '37', 'H8': '2', 'H9': '3', 'H2': '9',
+         'H3': '5', 'H1': '4', 'H6': '17', 'H7': '8', 'H4': '17', 'H5': '6', 'D8': '8', 'D9': '6', 'D6': '279',
+         'D7': '34', 'D4': '237', 'D5': '347', 'D2': '1', 'D3': '79', 'D1': '5'},
+        {'I6': '4', 'H9': '3', 'I2': '6', 'E8': '1', 'H3': '5', 'H7': '8', 'I7': '1', 'I4': '8', 'H5': '6', 'F9': '7',
+         'G7': '6', 'G6': '3', 'G5': '2', 'E1': '8', 'G3': '1', 'G2': '8', 'G1': '7', 'I1': '23', 'C8': '5', 'I3': '23',
+         'E5': '347', 'I5': '5', 'C9': '1', 'G9': '5', 'G8': '4', 'A1': '1', 'A3': '4', 'A2': '237', 'A5': '9',
+         'A4': '2357', 'A7': '27', 'A6': '257', 'C3': '8', 'C2': '237', 'C1': '23', 'E6': '579', 'C7': '9', 'C6': '6',
+         'C5': '37', 'C4': '4', 'I9': '9', 'D8': '8', 'I8': '7', 'E4': '6', 'D9': '6', 'H8': '2', 'F6': '125',
+         'A9': '8', 'G4': '9', 'A8': '6', 'E7': '345', 'E3': '79', 'F1': '6', 'F2': '4', 'F3': '23', 'F4': '1235',
+         'F5': '8', 'E2': '3', 'F7': '35', 'F8': '9', 'D2': '1', 'H1': '4', 'H6': '17', 'H2': '9', 'H4': '17',
+         'D3': '79', 'B4': '27', 'B5': '1', 'B6': '8', 'B7': '27', 'E9': '2', 'B1': '9', 'B2': '5', 'B3': '6',
+         'D6': '279', 'D7': '34', 'D4': '237', 'D5': '347', 'B8': '3', 'B9': '4', 'D1': '5'}
+        ]
+    print("Possible Solutions(1):")
+    Board(values).display(possible_solutions_1[0])
+    Board(values).display(possible_solutions_1[1])
+
     return values
+'''
